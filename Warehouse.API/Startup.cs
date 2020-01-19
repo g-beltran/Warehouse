a2515be68a4ac8c64ee85c31fff792acb8550f2e
Gerson Beltrán <beltrg01@stepstone.com>
2020-01-19 10:40:16 -0600

    Completed JWT Configuration Deleted unnecesary Class

--- Warehouse.API/Startup.cs
+++ Warehouse.API/Startup.cs
@@ -11,6 +11,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using Warehouse.Infrastructure.Data;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 namespace Warehouse.API
 {
@@ -30,6 +33,25 @@ namespace Warehouse.API
             services.AddDbContext<WarehouseDBContext>(opt=>
             opt.UseSqlServer(Configuration.GetConnectionString("WarehouseDBConnection"))
             );
+
+            var securityKey = Configuration.GetValue<string>("Security:SecurityKey");
+            var symetricSecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(opt =>
+                {
+                    opt.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = Configuration.GetValue<string>("Security:ValidIssuer"),
+                        ValidAudience = Configuration.GetValue<string>("Security:ValidAudience"),
+                        IssuerSigningKey = symetricSecurityKey
+                    };
+                });
+            services.AddSingleton(Configuration);
+                
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,6 +63,8 @@ namespace Warehouse.API
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
