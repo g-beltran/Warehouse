268f02837169714ead02924d18655a0d5f392c76
Gerson Beltrán <beltrg01@stepstone.com>
2020-01-19 01:15:39 -0600

    Implemented JWT to Authenticate and Authorize the API

---
+++ Warehouse.API/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Warehouse.API.Security;
+using Warehouse.Infrastructure.Data;
+
+namespace Warehouse.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private readonly WarehouseDBContext _context;
+        private readonly IConfiguration _configuration;
+
+        public LoginController(WarehouseDBContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpPost("Auth")]
+        public ActionResult Authorize()
+        {
+            var userName = Request.Form["userName"].ToString();
+            var password = Request.Form["password"].ToString();
+
+            var tokenProvider = new JWTProvider(_context, _configuration);
+            var token = tokenProvider.GetToken(userName, password);
+
+            if (token == null)
+            {
+                return BadRequest();
+            }
+
+            return Ok(token);
+        }
+    }
+}
---
+++ Warehouse.Infrastructure/Data/Enumerations.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Warehouse.Infrastructure.Data
+{
+    public static class Enumerations
+    {
+        public enum Roles
+        {
+            Administrator = 1,
+            Standard = 2
+        }
+
+        public enum OrderStatus
+        {
+            Active = 1,
+            Canceled = 2
+        }
+    }
+}
