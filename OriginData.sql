6b8fbf0d0f436126655d6a5c15fbdeb3f76b36fe
g-beltran <60051754+g-beltran@users.noreply.github.com>
2020-01-19 23:17:54 -0600

    Files required to Test the Solutions
    Please read Readme file

---
+++ OriginData.sql
@@ -0,0 +1,95 @@
+--USERS----------------------------------------------
+
+INSERT INTO [User] (UserName, Password, Role)
+Values('admin','admin',1);
+
+INSERT INTO [User] (UserName, Password, Role)
+Values('test1','test1',2);
+
+INSERT INTO [User] (UserName, Password, Role)
+Values('test3','test3',2);
+
+--ITEMS----------------------------------------------
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('LGTV001','LG TV Model 001', 100);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('LGTV002','LG TV Model 002', 200);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('LGTV003','LG TV Model 003', 250);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('SMTV001','Samsung TV Model 001', 100);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('SMTV002','Samsung TV Model 002', 200);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('SMTV003','Samsung TV Model 003', 250);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('VZTV001','Vizio TV Model 001', 100);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('VZTV002','Vizio TV Model 002', 200);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('VZTV003','Vizio TV Model 003', 250);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('TCTV001','TCL TV Model 001', 100);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('TCTV002','TCL TV Model 002', 200);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('TCTV003','TCL TV Model 003', 250);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('BOSB001','Bose Soundbar Model 001', 100);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('BOSB002','Bose Soundbar Model 002', 200);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('BOSB003','Bose Soundbar Model 003', 250);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('SOSB001','Sonos Soundbar Model 001', 100);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('SOSB002','Sonos Soundbar Model 002', 200);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('SOSB003','Sonos Soundbar Model 003', 250);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('AISB001','Aiiwa Soundbar Model 001', 100);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('AISB002','Aiiwa Soundbar Model 002', 200);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('AISB003','Aiiwa Soundbar Model 003', 250);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('JBSB001','JBL Soundbar Model 001', 100);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('JBSB002','JBL Soundbar Model 002', 200);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('JBSB003','JBL Soundbar Model 003', 250);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('MSVG001','Xbox One S', 10);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('MSVG002','Xbox One X', 5);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('SNVG001','Playstaion 4 Slim', 20);
+
+INSERT INTO Item (SKU, [Description],StockQuantity ) 
+VALUES ('SNVG002','Playstation 4 Pro', 15);
